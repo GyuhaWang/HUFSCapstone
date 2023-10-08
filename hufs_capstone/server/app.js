d48b27af7ff1d84309ad5ee0a8c7cb0cb1cfb7e6
MYUNGGWAN LEE <lmk960201@gmail.com>
2023-10-08 23:49:55 +0900

    update: app.js

--- hufs_capstone/server/app.js
+++ hufs_capstone/server/app.js
@@ -2,21 +2,23 @@ const express = require("express");
 const path = require("path");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
+const logger = require("morgan");
 
 const app = express();
 const port = 8000;
 
+app.use(logger("dev"));
 app.use(express.json());
 var cors = require("cors");
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "flutter_views")));
 
+app.set("views", path.join(__dirname, "flutter_views"));
 app.set("view engine", "dart");
-app.set("views", __dirname + "/lib");
-app.use(express.static(__dirname + "/lib"));
 
 app.get("/", (req, res) => res.render("main"));
 
