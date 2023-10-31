43f172fd32c33f3c4634bbd36760a64530c3bb05
MYUNGGWAN LEE <lmk960201@gmail.com>
2023-10-31 17:01:55 +0900

    update: app.js

--- hufs_capstone/server/app.js
+++ hufs_capstone/server/app.js
@@ -1,11 +1,18 @@
+// module
 const express = require("express");
 const path = require("path");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 
+// app
 const app = express();
-const port = 8000;
+
+// router
+const { Sequelize } = require("./models/index");
+const indexRouter = require("./routes/index");
+const petRouter = require("./routes/pet");
+const accelRouter = require("./routes/accel");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -21,9 +28,10 @@ const sequelize = new Sequelize("database", "username", "password", {
 
 // Test the database connection
 sequelize
+  .sync({ force: false })
   .authenticate()
   .then(() => {
-    console.log("Database connection has been established successfully.");
+    console.log("Database 연결 성공");
   })
   .catch((err) => {
     console.error("Unable to connect to the database:", err);
@@ -31,12 +39,26 @@ sequelize
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "flutter_views")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "flutter_views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => res.render("main"));
+// port
+const port = 8000;
+
+// router setting
+app.use("/", indexRouter);
+app.use("/pet", petRouter);
+app.use("/upload", accelRouter);
+app.use("/resulte", resulteRouter);
+
+// unidentified routes
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
 
 app.listen(port, () =>
   console.log(`App listening at http://localhost:${port}`)
