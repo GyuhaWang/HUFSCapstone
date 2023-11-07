fb5ccdbac148eec2008750bffd5238d3e81138b6
GyuhaWang <ethanleast@gmail.com>
2023-11-08 00:16:45 +0900

    Refactoring

---
+++ hufs_capstone/lib/pet_info/data/pet_info_model.dart
@@ -0,0 +1,33 @@
+class PetInfoModel {
+  final int userId;
+  final int age;
+  final int weight;
+  final String name;
+  final String type;
+
+  const PetInfoModel(
+      {required this.userId,
+      this.age = 0,
+      this.weight = 0,
+      this.name = "",
+      this.type = ""})
+      : super();
+
+  PetInfoModel.fromJson(Map<String, dynamic> json)
+      : userId = json['UserId'],
+        age = json['age'],
+        weight = json['weight'],
+        name = json['name'],
+        type = json['type'];
+
+  Map<String, dynamic> toJson() => {
+        'UserId': userId,
+        'age': age,
+        'weight': weight,
+        'name': name,
+        'type': type
+      };
+  bool isEmpty() {
+    return userId == '-1';
+  }
+}
