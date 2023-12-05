6eec94b348d87fa5285f30132a0e5d7472c924e0
MYUNGGWAN LEE <lmk960201@gmail.com>
2023-12-05 16:08:01 +0900

    update: pet api error fix

--- hufs_capstone/server/presenter/P_api.js
+++ hufs_capstone/server/presenter/P_api.js
@@ -4,7 +4,7 @@ const tf = require("@tensorflow/tfjs-node");
 
 const controller = {
   // Controller to store pet information
-  pet: async (req, res) => {
+  pet: async (req, expressRes) => {
     // Extract pet information from the request body
     const { name, weight, birth, type } = req.body;
     console.log(name, weight, birth, type);
@@ -16,26 +16,26 @@ const controller = {
       birth,
       type,
     })
-      .then((res) => {
+      .then((dbRes) => {
+        console.log(dbRes);
         // Send a success response
-        // res.status(201).json({ message: "반려동물 정보 저장 성공" });
-        console.log(res);
+        expressRes.status(201).json({ message: "반려동물 정보 저장 성공" });
       })
       .catch((error) => {
         // Handle errors and send an error response
         console.error(error);
-        res
+        expressRes
           .status(500)
           .json({ error: "반려동물의 정보를 저장하지 못했습니다." });
       });
   },
 
   // Controller to handle CSV file upload
-  upload: async (req, res) => {
+  upload: async (req, expressRes) => {
     try {
       // Check if a file is included in the request
       if (!req.file) {
-        return res.status(400).json({ error: "CSV 파일이 없습니다." });
+        return expressRes.status(400).json({ error: "CSV 파일이 없습니다." });
       }
 
       // Process the CSV data and save it in the database
@@ -48,12 +48,12 @@ const controller = {
         })
         .on("end", async () => {
           await AccelerationData.bulkCreate(rows);
-          res.status(201).json({ message: "CSV 업로드 & DB 저장 성공" });
+          expressRes.status(201).json({ message: "CSV 업로드 & DB 저장 성공" });
         });
     } catch (error) {
       // Handle errors and send an error response
       console.error("Error uploading and processing CSV file:", error);
-      res.status(500).json({ error: "CSV 파일을 저장하지 못했습니다." });
+      expressRes.status(500).json({ error: "CSV 파일을 저장하지 못했습니다." });
     }
   },
 
